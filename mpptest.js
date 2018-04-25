0dda75cf26e22ab3b109b158fdd2bb473641d7fc
Lamp <ledlamp89@gmail.com>
2018-04-24 18:26:16 -0700

    derp

--- mpptest.js
+++ mpptest.js
@@ -1,5 +1,7 @@
 var MPPclient = require("mpp-client");
 var Client = new MPPclient("ws://www.multiplayerpiano.com");
+Client.setChannel("lobby");
+Client.start();
 Client.on("connect", function(){
 	Client.sendArray([{m:'userset', set:{name:'\ufffc'}}]);
 });
